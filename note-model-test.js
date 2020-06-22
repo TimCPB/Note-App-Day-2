229503454b27e869bb7e763a3c9e414ca0601584
Tim <TimCPB@github.com>
2020-06-22 15:26:26 +0100

    created assert.js with var assert and #isTrue function

--- note-model-test.js
+++ note-model-test.js
@@ -1,15 +1,8 @@
-(function(exports) {
+// (function(exports) {
   function testNoteHasText() {
     var note = new Note("text");
 
-    if (note.getText() !== "text") {
-      throw new Error("wrong text");
-    }
-
-    if (note.getText() === "text") {
-      console.log("winners")
-    }
+    assert.isTrue(note.getText() == "text");
   };
-
   testNoteHasText();
-})(this);
+// })(this);
