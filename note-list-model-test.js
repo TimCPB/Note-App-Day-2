2592def14380bb3af809c63e2367376f89ead475
Rachael <rachaelewins@hotmail.com>
2020-06-22 16:02:55 +0100

    getNotes, returns an Array

--- note-list-model-test.js
+++ note-list-model-test.js
@@ -1,4 +1,5 @@
 // Stores an array of note models.
+
 // Has a method that will return all the note models stored in the array.
 // Has a method that creates and stores a new single note model.
 // This function takes as an argument a string that will be the value of the text property of the note
@@ -8,5 +9,7 @@ function testNoteList() {
   var noteList = new NoteList;
 
   assert.isTrue(noteList);
+
+  assert.isTrue(Array.isArray(noteList.getNotes()));
 };
 testNoteList();
--- note-list-model.js
+++ note-list-model.js
@@ -2,5 +2,9 @@
   function NoteList() {
   };
 
+  NoteList.prototype.getNotes = function() {
+    return []
+  }
+
   exports.NoteList = NoteList;
 })(this);
