f2ba2f917bbc61ffe44541cf7302ef92dc633e5e
Tim <TimCPB@github.com>
2020-06-25 16:01:33 +0100

    passing 'testNoteHasID'

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,6 +1,15 @@
+
 function testNoteHasText() {
   var note = new Note("text");
 
   assert.isTrue(note.getText() == "text");
 };
+
+function testNoteHasID(){
+  var note = new Note("test", 0)
+  assert.isTrue(note.id === 0)
+
+};
+
 testNoteHasText();
+testNoteHasID();
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -2,6 +2,7 @@
 (function(exports) {
   function NoteList() {
     this.noteModels = []
+    // this.id = 0
   };
 
   NoteList.prototype.getNotes = function() {
@@ -10,7 +11,9 @@
 
   NoteList.prototype.createNote = function(text) {
     var note = new Note(text);
+    // note.id += 1
     this.noteModels.push(note);
+    // note.id = this.noteModels.length
   }
 
   exports.NoteList = NoteList;
